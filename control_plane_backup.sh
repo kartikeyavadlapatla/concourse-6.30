220a60d587e66ac3a25cb079aec9004bf5e145b1
kartikeyavadlapatla <vkartheek.ky1311@gmail.com>
2020-08-04 09:43:43 -0400

    Update control_plane_backup.sh

--- control_plane_backup.sh
+++ control_plane_backup.sh
@@ -10,8 +10,15 @@ director_backup_status=false
 aws_s3_access_key=
 aws_s3_secret_key=
 aws_s3_region=us-east-1
-aws_s3_bucket=tile-storage-bucket-bcbsma
-
+aws_s3_bucket=
+BOSH_CLIENT_SECRET=
+BBR_SECRET=
+DIRECTOR_IP=
+CREDHUB_DB=
+OM_TARGET=
+OM_USER=
+OM_PWD=
+OM_SKIP_SSL_VALIDATION=true
 mkdir -p ${log_dir} ${backup_dir}
 
 echo "BACKUPS ARE TAKEN @ $timestamp"
@@ -19,22 +26,25 @@ echo "BACKUPS ARE TAKEN @ $timestamp"
 export AWS_ACCESS_KEY_ID=${aws_s3_access_key}
 export AWS_SECRET_ACCESS_KEY=${aws_s3_secret_key}
 export AWS_DEFAULT_REGION=${aws_s3_region}
-
+export OM_TARGET=${OM_TARGET}
+export OM_USERNAME=${OM_USER}
+export OM_PASSWORD=${OM_PWD}
+export OM_SKIP_SSL_VALIDATION=${OM_SKIP_SSL_VALIDATION}
 ##### Deleting Backups ##################
 
 delete_backup()
 {
         local backups_tobe_retained=$1
         echo "Backups to be retained: $backups_tobe_retained"
-        total_backups=$( /bin/ls -ltr /home/ubuntu/Control_Plane_BBR/control_plane_backups | /bin/grep -v total | /usr/bin/awk '{print $NF}' | /usr/bin/wc -l )
+        total_backups=$( /bin/ls -ltr /home/ubuntu/control-plane-backups/control_plane_backups | /bin/grep -v total | /usr/bin/awk '{print $NF}' | /usr/bin/wc -l )
         echo "Total Backups: ${total_backups}"
         if [[ ${total_backups} > ${backups_tobe_retained} ]]; then
                 backups_tobe_deleted=$((${total_backups}-${backups_tobe_retained}))
                 echo "Backups to be Deleted: $backups_tobe_deleted"
-                for old_backup in $( /bin/ls -ltr /home/ubuntu/Control_Plane_BBR/control_plane_backups | /bin/grep -v total | /usr/bin/awk '{print $NF}' | /usr/bin/head -${backups_tobe_deleted})
+                for old_backup in $( /bin/ls -ltr /home/ubuntu/control-plane-backups/control_plane_backups | /bin/grep -v total | /usr/bin/awk '{print $NF}' | /usr/bin/head -${backups_tobe_deleted})
                 do
-                        echo "$(/bin/date) removing /home/ubuntu/Control_Plane_BBR/control_plane_backups/${old_backup}"
-                        /bin/rm -Rf /home/ubuntu/Control_Plane_BBR/control_plane_backups/${old_backup}
+                        echo "$(/bin/date) removing /home/ubuntu/control-plane-backups/control_plane_backups/${old_backup}"
+                        /bin/rm -Rf /home/ubuntu/control-plane-backups/control_plane_backups/${old_backup}
                 done
         else
                 echo "Avaialble backups and backups to be retained count is same, no cleanup needed"
@@ -44,7 +54,8 @@ delete_backup()
 
 echo "Exporting OpsMan installation settings"
 
-om -e env export-installation --output-file ${backup_dir}/installation_${timestamp}
+#om -e env export-installation --output-file ${backup_dir}/installation_${timestamp}a
+om  export-installation --output-file ${backup_dir}/installation_${timestamp}
 
 /usr/local/bin/aws s3 cp ${backup_dir}/installation_${timestamp} s3://${aws_s3_bucket}/installation_${timestamp}
 /bin/chmod -R 777 ${backup_dir}/installation_${timestamp}
@@ -55,7 +66,9 @@ echo "Backing up Control Plane director with BBR"
 
 echo "Running Pre-Backup-Checks"
 
-/usr/bin/bbr director --private-key-path private_key.pem --username bbr --host 10.0.1.21 --debug pre-backup-check > ${log_director}
+/usr/bin/bbr director --private-key-path \
+        private_key.pem --username bbr --host ${DIRECTOR_IP} \
+        --debug pre-backup-check > ${log_director}
 
 /bin/grep -q "Director can be backed up." ${log_director}
 
@@ -68,7 +81,9 @@ fi
 
 echo "Taking Director Backup"
 
-bbr director --private-key-path private_key.pem --username bbr --host 10.0.1.21 --debug backup --artifact-path ${backup_dir} >> ${log_director} 2>&1
+/usr/bin/bbr director --private-key-path private_key.pem \
+        --username bbr --host ${DIRECTOR_IP} \
+        --debug backup --artifact-path ${backup_dir} >> ${log_director} 2>&1
 /bin/chmod -R 777 ${backup_dir}
 if [[ $? != 0 ]]; then
         echo "Err: Director backup failed"
@@ -77,12 +92,12 @@ if [[ $? != 0 ]]; then
         director_backup_status=false
 else
         director_backup_status=true
-        files=$(/bin/ls ${backup_dir}/10.0.1.21*)
+        files=$(/bin/ls ${backup_dir}/${DIRECTOR_IP}*)
         IFS=$'\n'$'\r'
-        PATH=$(/bin/ls ${backup_dir}/ | /bin/grep 10.0.1.21)
+        PATH=$(/bin/ls ${backup_dir}/ | /bin/grep ${DIRECTOR_IP})
         for j in ${files}
         do
-           /usr/local/bin/aws s3 cp ${backup_dir}/10.0.1.21_*/${j} s3://${aws_s3_bucket}/${backup_dir}/${PATH}/${j}
+           /usr/local/bin/aws s3 cp ${backup_dir}/${DIRECTOR_IP}_*/${j} s3://${aws_s3_bucket}/${backup_dir}/${PATH}/${j}
         done
         /usr/local/bin/aws s3 cp ${log_dir}/log_director_${timestamp}.log s3://${aws_s3_bucket}/${log_director}
 fi
@@ -95,7 +110,7 @@ echo "Cleaning up Director locks"
 
 /usr/bin/bbr director --private-key-path \
         private_key.pem --username bbr \
-        --host 10.0.1.21 \
+        --host ${DIRECTOR_IP} \
         --debug backup-cleanup >> ${log_director} 2>&1
 
 echo "Director backup-clean-up log location     @log_director:${log_director}"
@@ -107,11 +122,12 @@ fi
 #########################################################
 ########## Concourse backup pre-deployment checks #######
 
-export BOSH_CLIENT=ops_manager BOSH_CLIENT_SECRET=mq_xW-JKPxcQ8A3xWUkbrjg9Yvt7fWA2 BOSH_CA_CERT=~/root.cert BOSH_ENVIRONMENT=10.0.1.21
+export BOSH_CLIENT=ops_manager BOSH_CLIENT_SECRET=${BOSH_CLIENT_SECRET} BOSH_CA_CERT=~/root.cert BOSH_ENVIRONMENT=${DIRECTOR_IP}
 
 #bosh -d concourse manifest > $backup_dir/deployed-manifest-$timestamp.yml
 
-/usr/bin/bbr deployment --target 10.0.1.21 --deployment concourse --username bbr_client --password l76J6xyVh5DiD4jPWTBmeswwgxg-WEGX --ca-cert ~/root.cert --debug pre-backup-check > ${log_concourse} 2>&1
+/usr/bin/bbr deployment --target ${DIRECTOR_IP} --deployment concourse \
+        --username bbr_client --password ${BBR_SECRET} --ca-cert ~/root.cert --debug pre-backup-check > ${log_concourse} 2>&1
 
 /bin/grep -q "Deployment 'concourse' can be backed up." ${log_concourse}
 
@@ -125,7 +141,9 @@ fi
 ######### Concourse Backup ################################
 echo "Taking concourse Backup"
 
-/usr/bin/bbr deployment --target 10.0.1.21 --deployment concourse --username bbr_client --password l76J6xyVh5DiD4jPWTBmeswwgxg-WEGX --ca-cert ~/root.cert --debug backup --with-manifest --artifact-path ${backup_dir} >> ${log_concourse} 2>&1
+/usr/bin/bbr deployment --target ${DIRECTOR_IP} --deployment concourse \
+        --username bbr_client --password ${BBR_SECRET} --ca-cert ~/root.cert --debug backup --with-manifest \
+        --artifact-path ${backup_dir} >> ${log_concourse} 2>&1
 /bin/chmod -R 777 ${backup_dir}
 if [[ $? != 0 ]];then
         echo "Err: Concourse backup failed, check logs ${log_concourse} for details"
@@ -148,7 +166,8 @@ echo "concourse backup location                 @backup_dir:$backup_dir"
 
 ###### Cleaning up concourse locks ########################
 
-/usr/bin/bbr deployment --target 10.0.1.21 --deployment concourse --username bbr_client --password l76J6xyVh5DiD4jPWTBmeswwgxg-WEGX --ca-cert ~/root.cert --debug backup-cleanup >> ${log_concourse} 2>&1
+/usr/bin/bbr deployment --target ${DIRECTOR_IP} --deployment concourse \
+        --username bbr_client --password ${BBR_SECRET} --ca-cert ~/root.cert --debug backup-cleanup >> ${log_concourse} 2>&1
 
 echo "concourse backup clean-up log location @log_concourse:$log_concourse"
 
@@ -165,7 +184,7 @@ export CONCOURSE_CA_CERT="$(/usr/bin/credhub get -n /p-bosh/concourse/atc_tls -k
 unset CREDHUB_SECRET CREDHUB_CLIENT CREDHUB_SERVER CREDHUB_PROXY CREDHUB_CA_CERT
 
 /usr/bin/credhub login \
-  --server "https://10.0.1.28:8844" \
+  --server "https://${CREDHUB_DB}:8844" \
   --client-name=credhub_admin \
   --client-secret="${CONCOURSE_CREDHUB_SECRET}" \
   --ca-cert "${CONCOURSE_CA_CERT}"
